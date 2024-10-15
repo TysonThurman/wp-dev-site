f11bc8bb0bc2479ecc79eab1b60742b0388c9737
Thurman <Tyson.Thurman@ngc.com>
2024-10-15 07:17:36 -0700

    added template parts

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -1,17 +1,10 @@
 <?php get_header();
 
 while (have_posts()) {
-  the_post(); ?>
+  the_post(); 
+  pageBanner();
+  ?>
 
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>
-  </div>
   <div class="container container--narrow page-section">
     <div class="generic-content">
       <div class="row group">
@@ -23,6 +16,7 @@ while (have_posts()) {
         </div>
       </div>
     </div>
+
     <?php
 
     $relatedPrograms = get_field('related_programs');
