a6eb94573ea937826b75d175f406f1d0494a3f70
Thurman <Tyson.Thurman@ngc.com>
2024-10-17 12:15:48 -0700

    added synchronous searching

--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -3,11 +3,17 @@ import $ from 'jquery';
 class Search {
     // 1. Describe and create/initiate our object
     constructor() {
+        this.addSearchHTML();
+        this.resultsDiv = $("#search-overlay__results");
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
         this.events();
         this.isOverlayOpen = false;
+        this.isSpinnerVisible = false;
+        this.previousValue;
+        this.typingTimer;
     }
 
     // 2. Events
@@ -15,12 +21,15 @@ class Search {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
+        this.searchField.on("keyup", this.typingLogic.bind(this));
     }
 
     // 3. Methods (function, action...)
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
+        this.searchField.val('');
+        setTimeout(() => this.searchField.focus(), 300);
         this.isOverlayOpen = true;
     }
 
@@ -31,13 +40,62 @@ class Search {
     }
 
     keyPressDispatcher(e) {
-        if (e.keyCode == 83 && !this.isOverlayOpen) {
+        if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) {
             this.openOverlay();
         }
 
         if (e.keyCode == 27 && this.isOverlayOpen) {
             this.closeOverlay();
         }
+    }
+
+    typingLogic() {
+        if(this.searchField.val() != this.previousValue) {
+            clearTimeout(this.typingTimer);
+            if(this.searchField.val()) {
+                if(!this.isSpinnerVisible) {
+                    this.resultsDiv.html('<div class="spinner-loader"></div>');
+                    this.isSpinnerVisible = true;
+                }
+                this.typingTimer = setTimeout(this.getResults.bind(this), 750);
+            } else {
+                this.resultsDiv.html('');
+                this.isSpinnerVisible = false;
+            }
+        }
+        this.previousValue = this.searchField.val();
+    }
+
+    getResults() {
+        $.getJSON(`${universityData.root_url}/wp-json/wp/v2/posts?search=${this.searchField.val()}`, (posts) => {
+            $.getJSON(`${universityData.root_url}/wp-json/wp/v2/pages?search=${this.searchField.val()}`, (pages) => {
+                var combinedResults = posts.concat(pages);
+                this.resultsDiv.html(`
+                    <h2 class="search-overlay__section-title">General Information</h2>
+                     ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>' }
+                        ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+                     ${combinedResults.length ? '</ul>' : ''}
+                `);
+                this.isSpinnerVisible = false;
+            } );
+        });
+    }
+
+    addSearchHTML() {
+        $("body").append(`
+            <div class="search-overlay">
+                <div class="search-overlay__top">
+                    <div class="container">
+                    <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                    <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
+                    <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="container">
+                    <div id="search-overlay__results"></div>
+                </div>
+            </div>
+            `);
     }
 }
 
